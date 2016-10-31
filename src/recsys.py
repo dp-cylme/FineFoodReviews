8b6d889cae4ee7c639b05d84aad703bd2fb2b5f4
dpetrov <dpetrov@cyl.me>
2016-10-31 12:00:46 +0300

    Fix sparse matrix building

--- src/recsys.py
+++ src/recsys.py
@@ -3,10 +3,8 @@
 
 import numpy
 import pandas
-import functools
 import scipy
 
-from typing import Any
 from lightfm import LightFM
 
 
@@ -25,31 +23,46 @@ class RecSys(object):
 
         self.__model = LightFM(learning_rate=0.05, loss='bpr')
 
-    def __df_to_sparsematrix(self, df: pandas.DataFrame):
-        def add_smth(smth_dict, smth_num, smth):
-            if smth not in smth_dict:
-                smth_dict[smth] = smth_num
-                smth_num += 1
-            return smth_dict[smth]
-
         self.__users = {}
-        current_user_num = 0
-        get_user = functools.partial(add_smth, self.__users, current_user_num)
+        self.__current_user_num = 0
 
         self.__items = {}
-        current_item_num = 0
-        get_item = functools.partial(add_smth, self.__items, current_item_num)
+        self.__current_item_num = 0
 
-        sparsematrix = scipy.sparse.dok_matrix((df[self.__user].value_counts(
-        ).shape[0], df[self.__item].value_counts().shape[0]))
+    def get_user(self, user):
+        if user not in self.__users:
+            self.__users[user] = self.__current_user_num
+            self.__current_user_num += 1
+
+        return self.__users[user]
+
+    def get_item(self, item):
+        if item not in self.__items:
+            self.__items[item] = self.__current_item_num
+            self.__current_item_num += 1
+
+        return self.__items[item]
+
+    def __df_to_sparsematrix(self, df:
+                             pandas.DataFrame) -> scipy.sparse.coo.coo_matrix:
+        sparsematrix = scipy.sparse.dok_matrix(
+            (df[self.__user].value_counts().shape[0],
+             df[self.__item].value_counts().shape[0]),
+            dtype=numpy.int32)
 
         for _, row in df[[self.__user, self.__item, self.__rating]].iterrows():
-            sparsematrix[get_user(row[0])][get_item(row[1])] = row[2]
+            sparsematrix[self.get_user(row[0]), self.get_item(row[1])] = row[2]
 
-        return sparsematrix
+        return sparsematrix.tocoo(copy=True)
 
-    def fit(self, df: pandas.DataFrame) -> Any:
-        self.__model.fit(self.__df_to_sparsematrix(df), epochs=10)
+    def fit(self, df: pandas.DataFrame):
+        self.__model.fit(self.__df_to_sparsematrix(df), epochs=20)
 
-    def predict(self, users: numpy.array, items: numpy.array) -> numpy.array:
-        return self.__model.predict(users, items, num_threads=8)
+    def predict(self,
+                users: numpy.array,
+                items: numpy.array,
+                num_threads: int=1) -> numpy.array:
+        return self.__model.predict(
+            numpy.array([self.get_user(x) for x in users]),
+            numpy.array([self.get_item(x) for x in items]),
+            num_threads=num_threads)
